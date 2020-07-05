cddcf7c6745bf170ebdc203f1f234a1e1dd53e78
StephenBrooks220413 <stephen.brooks220413@gmail.com>
2020-07-05 13:18:45 -0700

    update

---
+++ ClearProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ClearProject.MvcApplication" Language="C#" %>
---
+++ ClearProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ClearProject.Startup))]
+namespace ClearProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
